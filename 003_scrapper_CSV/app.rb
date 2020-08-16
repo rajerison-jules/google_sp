77e298248d2c606e32cf6b2fc7886ae14d277a5a
rajerison-jules <julesrajerison@gmail.com>
2020-08-16 22:52:57 +0300

    final commit

--- 003_scrapper_CSV/app.rb
+++ 003_scrapper_CSV/app.rb
@@ -1,10 +1,12 @@
-require 'json'
+require 'csv'
 require "googleauth"
 
 $:.unshift File.expand_path('./../lib', __FILE__)
 require 'mairie'
 
 list_mairie = Mairie.new.afficeur
-File.open("db/email.json","w") do |f|
-  f.write(list_mairie.to_json)
-end 
+result = list_mairie.map{|tab| tab.map{|element| element.map{|k, v| [k, v]}}}
+result = result.map { |data| data.join(",") }.join("\n")
+File.open("./db/emails.csv", 'w') do |file|
+file.write(result)
+end
